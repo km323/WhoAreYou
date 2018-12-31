96593a43cd5f6140a2f3e56b805626f83121fb2b
ebonyCitizen <coco_coco87@hotmail.com>
2018-12-31 23:55:16 +0900

    fade in fade out

--- Assets/Script/SceneManager/SceneController.cs
+++ Assets/Script/SceneManager/SceneController.cs
@@ -42,21 +42,47 @@ public class SceneController : SingletonMonoBehaviour<SceneController> {
     private Scene nextScene = Scene.None;
     private Scene additiveScene = Scene.None;
 
+    //フェイド
+    private const float fadeInterval = 1f;
+    private bool isFading;
+    private float fadeAlpha;
+    private Color fadeColor;
+
     protected override void Awake()
     {
         base.Awake();
+
+        isFading = false;
+        fadeAlpha = 0;
+        fadeColor = Color.black;
 
         DontDestroyOnLoad(gameObject);
         gameObject.name = "SceneController";
     }
 
+    private void OnGUI()
+    {
+        if (isFading)
+        {
+            //色と透明度を更新して白テクスチャを描画 .
+            fadeColor.a = fadeAlpha;
+            GUI.color = this.fadeColor;
+            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), Texture2D.whiteTexture);
+        }
+    }
+
     public void Change(Scene scene, Scene additiveScene = Scene.None)
     {
+        if (isFading)
+            return;
+
         nextScene = scene;
 
         this.additiveScene = additiveScene;
 
-        SceneManager.LoadScene(SceneName[nextScene]);
+        StartCoroutine(ChangeScene(nextScene));
+
+        //SceneManager.LoadScene(SceneName[nextScene]);
 
         if (this.additiveScene != Scene.None)
             SceneManager.LoadScene(SceneName[this.additiveScene], LoadSceneMode.Additive);
@@ -66,5 +92,30 @@ public class SceneController : SingletonMonoBehaviour<SceneController> {
         nextScene = Scene.None;
         this.additiveScene = Scene.None;
     }
-    
+
+    IEnumerator ChangeScene(Scene scene)
+    {
+        //fade out
+        isFading = true;
+        float time = 0;
+        while (time <= fadeInterval)
+        {
+            fadeAlpha = Mathf.Lerp(0f, 1f, time / fadeInterval);
+            time += Time.unscaledDeltaTime;
+            yield return null;
+        }
+
+        //load scene
+        SceneManager.LoadScene(SceneName[scene]);
+
+        //fade in
+        time = 0;
+        while (time <= fadeInterval)
+        {
+            fadeAlpha = Mathf.Lerp(1f, 0f, time / fadeInterval);
+            time += Time.unscaledDeltaTime;
+            yield return null;
+        }
+        isFading = false;
+    }
 }
