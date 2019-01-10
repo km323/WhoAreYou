60d01d3586c2b02b512b21e9204ffe25bbe96a17
kato <miyakm323@gmail.com>
2019-01-10 15:03:37 +0900

    post effectの調整

--- Assets/Script/Tutorial/SlowMotionTutorial.cs
+++ Assets/Script/Tutorial/SlowMotionTutorial.cs
@@ -14,6 +14,7 @@ public class SlowMotionTutorial : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+        postCamera.SetActive(false);
         distance = transform.position - player.position;
     }
 	
