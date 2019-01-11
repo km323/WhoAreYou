f2fad98d7342fba42de2bba9fc855ed48f2405f0
kato <miyakm323@gmail.com>
2019-01-11 22:39:31 +0900

    枠をゲージにする

--- Assets/Script/Player/DodgeGauge.cs
+++ Assets/Script/Player/DodgeGauge.cs
@@ -7,71 +7,123 @@ using UnityEngine;
 /// </summary>
 
 public class DodgeGauge : MonoBehaviour {
-    [SerializeField]
-    protected SpriteRenderer frame;
-    [SerializeField]
-    protected Texture[] recMask;
+    //private GameObject[] gauge;
 
-    protected Texture curTexture;
-    protected Texture oldTexture;
+    //protected const float firstPhase = 1 / 4f;
+    //protected const float secondPhase = 2 / 4f;
 
-    protected const float firstPhase = 1 / 4f;
-    protected const float secondPhase = 2 / 4f;
-    protected const float thirdPhase = 3 / 4f;
+    //private StageManager stageManager;
+    //protected float pressedTime;
 
-    private StageManager stageManager;
-    protected float pressedTime;
 
-	// Use this for initialization
-	void Start () {
-        stageManager = FindObjectOfType<StageManager>();
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        UpdateMask();
-    }
+    //// Use this for initialization
+    //void Start()
+    //{
+    //    stageManager = FindObjectOfType<StageManager>();
+    //    gauge = GameObject.FindGameObjectsWithTag("Gauge");
+    //}
 
-    // 4段階 (過ぎてる時間の割合：どのテクスチャ番号）  
-    //0:0,　1/4:1,  2/4:2,  3/4:3,  1:4
-    private void UpdateMask()
-    {
-        pressedTime = PlayerController.GetPlayerInput().TouchTime;
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    UpdateMask();
+    //}
 
-        if (ReachNeedTime(stageManager.GetPressTimeNeed()))
-            curTexture = recMask[4];
-        else if (ReachNeedTime(stageManager.GetPressTimeNeed() * thirdPhase))
-            curTexture = recMask[3];
-        else if (ReachNeedTime(stageManager.GetPressTimeNeed() * secondPhase))
-            curTexture = recMask[2];
-        else if (ReachNeedTime(stageManager.GetPressTimeNeed() * firstPhase))
-            curTexture = recMask[1];
-        else
-            curTexture = recMask[0];
+    //// 4段階 (過ぎてる時間の割合：どのテクスチャ番号）  
+    ////0:0,　1/4:1,  2/4:2,  3/4:3,  1:4
+    //private void UpdateMask()
+    //{
+    //    pressedTime = PlayerController.GetPlayerInput().TouchTime;
 
-        SetTexture(curTexture);
-    }
+    //    if (ReachNeedTime(stageManager.GetPressTimeNeed()))
+    //        gauge[0].SetActive(true);
+    //    else if (ReachNeedTime(stageManager.GetPressTimeNeed() * secondPhase))
+    //        gauge[1].SetActive(true);
+    //    else if (ReachNeedTime(stageManager.GetPressTimeNeed() * firstPhase))
+    //        gauge[2].SetActive(true);
+    //    else
+    //    {
+    //        foreach (GameObject obj in gauge)
+    //            obj.SetActive(false);
+    //    }
+    //}
 
-    protected bool ReachNeedTime(float needTime)
-    {
-        if (pressedTime >= needTime)
-            return true;
-        else
-            return false;
-    }
+    //private bool ReachNeedTime(float needTime)
+    //{
+    //    if (pressedTime >= needTime)
+    //        return true;
+    //    else
+    //        return false;
+    //}
 
-    protected void SetTexture(Texture tex)
-    {
-        if (curTexture != oldTexture)
-        {
-            oldTexture = curTexture;
-            frame.material.SetTexture("_AlphaTex", tex);
-            if (tex == recMask[0])
-                return;
-            else if (tex == recMask[4])
-                SoundManager.Instance.PlaySe(SE.DodgeGaugeMax);
-            else
-                SoundManager.Instance.PlaySe(SE.DodgeGaugeCharge);
-        }
-    }
+
+
+    
+    //   [SerializeField]
+    //   protected SpriteRenderer frame;
+    //   [SerializeField]
+    //   protected Texture[] recMask;
+
+    //   protected Texture curTexture;
+    //   protected Texture oldTexture;
+
+    //   protected const float firstPhase = 1 / 4f;
+    //   protected const float secondPhase = 2 / 4f;
+    //   protected const float thirdPhase = 3 / 4f;
+
+    //   private StageManager stageManager;
+    //   protected float pressedTime;
+
+    //// Use this for initialization
+    //void Start () {
+    //       stageManager = FindObjectOfType<StageManager>();
+    //   }
+
+    //// Update is called once per frame
+    //void Update () {
+    //       UpdateMask();
+    //   }
+
+    //   // 4段階 (過ぎてる時間の割合：どのテクスチャ番号）  
+    //   //0:0,　1/4:1,  2/4:2,  3/4:3,  1:4
+    //   private void UpdateMask()
+    //   {
+    //       pressedTime = PlayerController.GetPlayerInput().TouchTime;
+
+    //       if (ReachNeedTime(stageManager.GetPressTimeNeed()))
+    //           curTexture = recMask[4];
+    //       else if (ReachNeedTime(stageManager.GetPressTimeNeed() * thirdPhase))
+    //           curTexture = recMask[3];
+    //       else if (ReachNeedTime(stageManager.GetPressTimeNeed() * secondPhase))
+    //           curTexture = recMask[2];
+    //       else if (ReachNeedTime(stageManager.GetPressTimeNeed() * firstPhase))
+    //           curTexture = recMask[1];
+    //       else
+    //           curTexture = recMask[0];
+
+    //       SetTexture(curTexture);
+    //   }
+
+    //   protected bool ReachNeedTime(float needTime)
+    //   {
+    //       if (pressedTime >= needTime)
+    //           return true;
+    //       else
+    //           return false;
+    //   }
+
+    //   protected void SetTexture(Texture tex)
+    //   {
+    //       if (curTexture != oldTexture)
+    //       {
+    //           oldTexture = curTexture;
+    //           frame.material.SetTexture("_AlphaTex", tex);
+    //           if (tex == recMask[0])
+    //               return;
+    //           else if (tex == recMask[4])
+    //               SoundManager.Instance.PlaySe(SE.DodgeGaugeMax);
+    //           else
+    //               SoundManager.Instance.PlaySe(SE.DodgeGaugeCharge);
+    //       }
+    //   }
 }
